a1050c7d8b5f0ea68363ce57a8790cdadc6f404c
matiasmaizares <72669598+matiasmaizares@users.noreply.github.com>
2021-05-11 10:20:58 -0300

    Creando public

---
+++ public/precache-manifest.eda4ca1efaa46b16391f3edfe3a0a9b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8326faef150225daaf22d496da4bd907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "60383bdddc7c96b0d234",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "2be79f90bcc8d65d7506",
+    "url": "/static/js/2.bd64b146.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.bd64b146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60383bdddc7c96b0d234",
+    "url": "/static/js/main.ab887471.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
